7efd32e72019a407177e2db101f30d6e063cac42
Yuan Guozheng <yuanguozheng@msn.cn>
2015-04-18 22:40:59 +0800

    Fix Upload Bug

--- 验证码/验证码/Model/ImageInfo.cs
+++ 验证码/验证码/Model/ImageInfo.cs
@@ -27,52 +27,38 @@ namespace 验证码.Model
             req.Method = "POST";
             req.ContentType = string.Format("multipart/form-data; boundary={0}", boundary);
 
-
-            var stream = File.Open("美女.jpg", FileMode.Open);
-            byte[] b = new byte[stream.Length];
-            stream.Read(b, 0, (int)stream.Length);
             //构建formdata
             //构建
-
-            StringBuilder sb = new StringBuilder();
-            sb.Append(beginBoundary + string.Format(key, "filesize") + "465664");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + string.Format(key, "Filename") + "美女.jpg");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + string.Format(key, "fileheight") + "0");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + string.Format(key, "filewidth") + "0");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + string.Format(key, "newfilesize") + "465664");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + string.Format(key, "compresstime") + "439");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + string.Format(key, "filetype") + ".jpg");
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + "Content-Disposition: form-data; name=\"filedata\"; filename=\"美女.jpg\"\r\nContent-Type: application/octet-stream");
-            sb.Append("\r\n\r\n");
-
-
-            byte[] buffer = new byte[sb.Length];
-            buffer = Encoding.Default.GetBytes(sb.ToString());
-            ms.Write(buffer, 0, buffer.Length);
-            ms.Write(b, 0, b.Length);
-
-            sb.Remove(0, sb.Length);
-            sb.Append("\r\n");
-            sb.Append(beginBoundary + "Content-Disposition: form-data; name=\"Upload\"");
-            sb.Append("\r\n\r\n");
-            sb.Append("Submit Query\r\n" + endBoundary);
-
-            byte[] buf = new byte[sb.Length];
-            buf = Encoding.Default.GetBytes(sb.ToString());
-            ms.Write(buf, 0, buf.Length);
-
-            var bufferms = new byte[ms.Length];
-            ms.Read(bufferms, 0, (int)ms.Length);
-            using (Stream s = req.GetRequestStream())
+            using (Stream reqStream = req.GetRequestStream())
             {
-                s.Write(bufferms, 0, bufferms.Length);
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(beginBoundary + string.Format(key, "filesize") + "465664");
+                sb.AppendLine(beginBoundary + string.Format(key, "Filename") + "美女.jpg");
+                sb.AppendLine(beginBoundary + string.Format(key, "fileheight") + "0");
+                sb.AppendLine(beginBoundary + string.Format(key, "filewidth") + "0");
+                sb.AppendLine(beginBoundary + string.Format(key, "newfilesize") + "465664");
+                sb.AppendLine(beginBoundary + string.Format(key, "compresstime") + "0");
+                sb.AppendLine(beginBoundary + string.Format(key, "filetype") + ".jpg");
+                sb.AppendLine(beginBoundary + "Content-Disposition: form-data; name=\"filedata\"; filename=\"美女.jpg\"\r\nContent-Type: application/octet-stream");
+                sb.Append("\r\n");
+
+                byte[] strStream = Encoding.UTF8.GetBytes(sb.ToString());
+
+                using (var stream = File.Open("美女.jpg", FileMode.Open))
+                {
+                    byte[] b = new byte[stream.Length];
+                    stream.Read(b, 0, (int)stream.Length);
+                    reqStream.Write(strStream, 0, strStream.Length);
+                    reqStream.Write(b, 0, b.Length);
+                }
+
+                sb = new StringBuilder();
+                sb.Append("\r\n");
+                sb.AppendLine(beginBoundary + "Content-Disposition: form-data; name=\"Upload\"");
+                sb.Append("\r\n");
+                sb.Append("Submit Query\r\n" + endBoundary);
+                byte[] bstrStream = Encoding.UTF8.GetBytes(sb.ToString());
+                reqStream.Write(bstrStream, 0, bstrStream.Length);
             }
 
             using (Stream s = req.GetResponse().GetResponseStream())
